07e31efcf25af8c920c452133f44ebc8e63fa366
jagadeesh03 <jagadeeshelumalai311@gmail.com>
2022-02-09 01:55:44 +0530

    Solved

--- Recursion/Repeat.java
+++ Recursion/Repeat.java
@@ -1,21 +1,40 @@
 import java.util.*;
 public class Repeat
 {
-public static void main(String[] args)
+public static void run()
 {
- int[] a1 = {1,2,2,3,4};
- int[] a2 = {1,2,3};
- ArrayList<Integer> list = new ArrayList<>();
- for(int i=0;i<a1.length;i++)
+// int[] a1 = {1,2,2,3,4,6};
+// int[] a2 = {1,2,3,4};
+// ArrayList<Integer> list = new ArrayList<>();
+ boolean[] prime = new boolean[15];
+ int i=0;
+ int j=0;
+ while(i < a1.length && j < a2.length)
  {
-  for(int j=0;j<a2.length;i++)
-  {
-     if(a1[i] != a2[j])
+     if(!prime[a1[i]])
      {
-       
+       if(a1[i] == a2[j])
+       {
+         prime[a1[i]] = true;
+       }
+         i++;
+         j++;
      }
-  }
+     else
+     {
+       i++;
+     }
  }
+ while(i < a1.length)
+  {
+    list.add(a1[i]);
+    i++; 
+  }
+  while(j < a2.length)
+  {
+    list.add(a2[j]);
+    j++; 
+  }
  System.out.println(list);
 }
 }
