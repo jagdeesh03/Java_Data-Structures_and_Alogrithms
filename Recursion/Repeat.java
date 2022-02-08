be7b760e9695d1d16c4d564504dae91d9e6447e9
jagadeesh03 <jagadeeshelumalai311@gmail.com>
2022-02-08 23:48:26 +0530

    Solved

---
+++ Recursion/Repeat.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Repeat
+{
+public static void main(String[] args)
+{
+ int[] a1 = {1,2,2,3,4};
+ int[] a2 = {1,2,3};
+ ArrayList<Integer> list = new ArrayList<>();
+ for(int i=0;i<a1.length;i++)
+ {
+  for(int j=0;j<a2.length;i++)
+  {
+     if(a1[i] != a2[j])
+     {
+       
+     }
+  }
+ }
+ System.out.println(list);
+}
+}
